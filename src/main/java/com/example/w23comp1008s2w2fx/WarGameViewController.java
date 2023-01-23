9b8763a2e8da134fe4175d4010bac600e96e82c0
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-23 15:20:43 -0500

    Added fx:id's to the view and controller files

--- src/main/java/com/example/w23comp1008s2w2fx/WarGameViewController.java
+++ src/main/java/com/example/w23comp1008s2w2fx/WarGameViewController.java
@@ -1,4 +1,27 @@
 package com.example.w23comp1008s2w2fx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class WarGameViewController {
+
+    @FXML
+    private Label faceNameLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    void nextCard(ActionEvent event) {
+
+    }
+
 }
