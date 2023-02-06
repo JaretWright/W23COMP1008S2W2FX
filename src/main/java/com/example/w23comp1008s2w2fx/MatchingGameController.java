9b487512d70103cd5a77dc7b4f00f3dbb1a4e821
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-06 15:10:50 -0500

    Week 5 - Added a matching game view and controller

---
+++ src/main/java/com/example/w23comp1008s2w2fx/MatchingGameController.java
@@ -0,0 +1,55 @@
+package com.example.w23comp1008s2w2fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class MatchingGameController {
+
+    @FXML
+    private ImageView cardImage0;
+
+    @FXML
+    private ImageView cardImage1;
+
+    @FXML
+    private ImageView cardImage2;
+
+    @FXML
+    private ImageView cardImage3;
+
+    @FXML
+    private ImageView cardImage4;
+
+    @FXML
+    private ImageView cardImage5;
+
+    @FXML
+    private ImageView cardImage6;
+
+    @FXML
+    private ImageView cardImage7;
+
+    @FXML
+    private Label correctLabel;
+
+    @FXML
+    private Label guessNumLabel;
+
+    @FXML
+    private Label percentCorrectLabel;
+
+    @FXML
+    private HBox row1HBox;
+
+    @FXML
+    private HBox row2HBox;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
+}
